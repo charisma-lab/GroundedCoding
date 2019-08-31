991ccd932dcfae0763e9aa276da3ddb6f0b45e3b
CHARISMA Lab <charisma@engr.oregonstate.edu>
2019-08-30 18:43:22 -0700

    Samar: we now have Session cookie for tracking start of trail

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, session, escape, request
 from db_connector import *
 from importlib import import_module
 import os
 
 webapp = Flask(__name__)
 db_connection = connect_to_database()
+#TODO: generate this unique everytime using os.urandom(16)
+webapp.secret_key = b'8\x9f\xf5\x83j\x9e\xaa\x83\x07+Ai\x9f\xd9\xc5_'
 
 # import camera driver
 if os.environ.get('CAMERA'):
@@ -36,7 +38,9 @@ def trail_action_record(action):
     VALUES
     (%s, %s, %s)
     """
-    data = (3.42, 2, action)
+    #TODO: calculate the timestamp
+    timestamp = session['start_time']
+    data = (timestamp, 2, action)
     execute_query(db_connection, query, data)
     return("Action recorded");
 
@@ -46,7 +50,6 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
 
 @webapp.route('/video_feed')
 def video_feed():
@@ -58,6 +61,12 @@ def video_feed():
 def index():
     """Video streaming home page."""
     return render_template('index.html')
+
+@webapp.route('/trail/start')
+def start_trail():
+    #TODO: Make this dynamic using utcnow()
+    session['start_time'] = '12342';
+    return session['start_time']
 
 if __name__ == '__main__':
     webapp.run(host='0.0.0.0', debug=False)
