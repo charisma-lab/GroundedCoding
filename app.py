b1b57d202eb9cba53ca8d6b46274e0f986d717ec
CHARISMA Lab <charisma@engr.oregonstate.edu>
2019-08-30 17:48:54 -0700

    Samar&Abrar: Working camera feed through flask!!!

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, Response
 from db_connector import *
+from importlib import import_module
+import os
 
 webapp = Flask(__name__)
 db_connection = connect_to_database()
+
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera_opencv import Camera
 
 @webapp.route("/")
 def main():
@@ -31,6 +39,25 @@ def trail_action_record(action):
     data = (3.42, 2, action)
     execute_query(db_connection, query, data)
     return("Action recorded");
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@webapp.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@webapp.route('/video')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
 if __name__ == '__main__':
     webapp.run(host='0.0.0.0', debug=False)
