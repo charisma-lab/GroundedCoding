a1730202f81fd9490e033bbc4872ddc419a9e4f7
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2019-09-22 22:34:02 -0700

    We can now see video stream of the laptop's camera from any device. This makes the web interface usable on multiple devices by displayng video stream from the server.
    We now display video using miguelgrinberg's code which uses OpenCV
    
    Removed references to the video streaming which used pure HTML+JS

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from importlib import import_module
 import os
 import time
 
+from camera_opencv import Camera
+
 webapp = Flask(__name__)
 db_connection = connect_to_database()
 #TODO: generate this unique everytime using os.urandom(16)
@@ -97,6 +99,20 @@ def stop_trail():
     session.pop('trail_id', None)
     session.pop('trail_start_time', None)
     return("Trail Stopped!")
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@webapp.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     webapp.run(host='0.0.0.0', ssl_context='adhoc', debug=False)
