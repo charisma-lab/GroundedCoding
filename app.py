c52ea8e1581835553950a08b384601e49f9d080d
Samarendra M Hedaoo <samarendra.hedaoo@gmail.com>
2019-11-11 10:56:11 -0800

    https to take care of browsers which disallow mediaRecorder API over http

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def stop_trail():
     return("Trail Stopped!")
 
 if __name__ == '__main__':
-    webapp.run(host='0.0.0.0', debug=False)
+    webapp.run(host='0.0.0.0', ssl_context='adhoc', debug=False)
